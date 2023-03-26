2d5cef2f7469d03461f5e819c924651c1682ff4b
Lizzie Prader <lrprader@gmail.com>
2023-03-25 22:35:10 -0700

    back to HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ParallaxProvider } from 'react-scroll-parallax';
 import Bio from "./pages/bio";
 import Contact from "./pages/contact";
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
     <ParallaxProvider>
     <div id="app">
       <NavBar 
@@ -32,6 +32,6 @@ root.render(
       </Routes>
     </div>
     </ParallaxProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
